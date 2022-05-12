119623d3c4fd8dbdc364224a9d0d5522774b0335
chasewhitfield <62225349+chasewhitfield@users.noreply.github.com>
2022-05-12 13:23:44 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,39 @@ import java.io.*;
 
 public class Main {
     public static void main (String [] args) throws FileNotFoundException {
-        ArrayList<Intern> TechStar = generateList("");
-        
-        Intern a = new Intern("Chase Whitfield", 5, 5, 5, 5, 5);
-        Intern b = new Intern("Ben Coffta", 3, 7, 4, 9, 4);
+        ArrayList<Intern> TechStar = generateList("intern.csv");
+
+        Intern a = new Intern("Chase Whitfield", 5, 5, 5, 5, 5, 5, 5,
+                5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+        , 5, 5);
+
+        List<Intern> partners = a.getClosestInterns(TechStar, 2);
+        for (Intern partner : partners) {
+            System.out.println(partner.generateEmail());
+            System.out.println("Distance from you: " + partner.distanceToOtherIntern(a));
+        }
     }
 
     static ArrayList<Intern> generateList(String filePath) throws FileNotFoundException {
         ArrayList<Intern> interns = new ArrayList<Intern>();
         File csv = new File(filePath); //might have to change to filepath
-        Scanner sc = null;
         try {
-            sc = new Scanner(csv);
+            Scanner sc = new Scanner(csv);
             while (sc.hasNextLine()) {
                 String curRow = sc.nextLine();
                 String[] cols = curRow.split(",");
-                Intern curIntern = new Intern(cols[0].concat(cols[1]), Integer.parseInt(cols[2]), Integer.parseInt(cols[3]), Integer.parseInt(cols[4]), Integer.parseInt(cols[5]), Integer.parseInt(cols[6]));
+                Intern curIntern = new Intern(cols[0], Integer.parseInt(cols[1]), Integer.parseInt(cols[2]), Integer.parseInt(cols[3]), Integer.parseInt(cols[4]), Integer.parseInt(cols[5]),
+                        Integer.parseInt(cols[6]), Integer.parseInt(cols[7]), Integer.parseInt(cols[8]), Integer.parseInt(cols[9]),
+                        Integer.parseInt(cols[10]), Integer.parseInt(cols[11]), Integer.parseInt(cols[12]), Integer.parseInt(cols[13]),
+                        Integer.parseInt(cols[14]), Integer.parseInt(cols[15]), Integer.parseInt(cols[16]), Integer.parseInt(cols[17]),
+                        Integer.parseInt(cols[18]), Integer.parseInt(cols[19]));
                 interns.add(curIntern);
-                System.out.println(curIntern.getName() + "has been added!");
+                System.out.println(curIntern.getName() + " has been added!");
             }
         }
         catch (FileNotFoundException e) {
             System.out.println("File not found");
             throw e;
-        } finally {
-            if (sc != null) {
-                sc.close();
-            }
         }
         return interns;
     }
